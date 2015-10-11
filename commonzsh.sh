71de7ade5431f00ebfd2ee99db92deee03ac9306
Sanjeya Cooray <sanjeya.cooray@gmail.com>
2015-10-11 14:51:17 +0200

    Added functions

--- commonzsh.sh
+++ commonzsh.sh
@@ -2,8 +2,12 @@
 DIR=$HOME/.dotfiles
 ALIASES=$DIR/system/aliases
 TOOLS=$DIR/tools
+FUNCITONS=$DIR/system/functions
+
 
 # Z frecent folder navigation
 . $TOOLS/z/z.sh
 
 . $ALIASES
+
+. $FUNCTIONS
