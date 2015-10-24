2aa8481999899bfb408316c8a92e940df5054663
sanjeyac <sanjeya.cooray@gmail.com>
2015-10-24 22:55:18 +0200

    Added speedtest

--- tools/installtools.sh
+++ tools/installtools.sh
@@ -8,3 +8,6 @@ git clone https://github.com/rupa/z.git
 #remove all git traces
 find . -name ".git*" -exec rm -rf {} \;
 
+#speedtest
+wget -O speedtest-cli https://raw.github.com/sivel/speedtest-cli/master/speedtest_cli.py
+chmod +x speedtest-cli
